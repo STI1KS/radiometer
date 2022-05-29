f363fa486595b6a743483e24ef3544c8dded6576
STI1KS <cimonovleshka@yandex.ru>
2022-05-29 15:41:19 +0300

    git add

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,16 @@
 #define buz 6                 // пин пищалки
 #define buz_delay 3           // время писка
 #define buz_power 10          // громкость
+
 #define LED 10                // пин светодиода
 #define LED_delay 10          // время мигания
 #define LED_power 100         // яркость
+
 #define screen_refr 1000      // период обновления экрана
 #define sens 13.33            // чувствительность датчика мкР/ч/CPS
+
+#define bat A3            // чувствительность датчика мкР/ч/CPS
+
 volatile int count = 0;       // число частиц
 volatile bool LED_flag = 0;   // флаг для светодиода
 volatile bool buz_flag = 0;   // флаг для пищалки
@@ -21,7 +26,7 @@ unsigned long last_delay = 0;      // время между последними
 unsigned long last_time = 0;       // время последнего импульса
 float result;
 float k = 0.1;
-GyverOLED<SSD1306_128x64, OLED_NO_BUFFER> oled;
+GyverOLED<SSD1306_128x64, OLED_NO_BUFFER>  oled;
 // GFilterRA analog0;  
 
 float expRunningAverageAdaptive(float newVal) {
@@ -57,7 +62,14 @@ void setup() {
   pinMode(LED ,OUTPUT);
   attachInterrupt(0,countPulse,FALLING); 
   last_time = micros();
+  // oled.dot(127, 63, 1);
+  oled.line(0, 55, 124, 55, 1);
+  oled.line(0, 63, 124, 63, 1);
+  oled.line(0, 55, 0, 63, 1);
+  oled.line(124, 55, 124, 63, 1);
+  oled.rect(125, 57, 127, 61, 1);
   
+  // oled.rect(1, 56, 124, 57, 0);
 }
 void loop() {
   if (millis() - timer >= screen_refr) {
@@ -66,7 +78,11 @@ void loop() {
     // oled.clear();       // очистка
     oled.home();        // курсор в 0,0
     // oled.print(sens*float(count)*1000/float(screen_refr));
+    oled.setScale(4);
     oled.print(result);
+    oled.setCursor(0,4);
+    oled.setScale(3);
+    oled.rect(2, 57, round(1.2*(66.66*((5.0*analogRead(bat)/1024.0)-2.75))-2), 61, 1);
     count = 0;
     do {
       timer += screen_refr;
